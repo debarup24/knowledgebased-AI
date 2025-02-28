4271cf951093a7ef58991b37165f87b20a4d4313
Debarup <87480898+debarup24@users.noreply.github.com>
2025-02-28 11:35:56 +0530

    Final commit

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ const Home = () => {
       <div className="flex flex-col">
         <header className="sticky top-0 z-10 flex h-[57px] bg-background items-center gap-1 border-b px-4">
           <h1 className="text-xl font-semibold text-red-500">
-            <span className="flex flex-row">Agent_AIsha</span>
+            <span className="flex flex-row">Agent_AIdoc</span>
           </h1>
           <div className="w-full flex flex-row justify-end gap-2">
              <ModeToggle />   {/*  Theme toggle */}
